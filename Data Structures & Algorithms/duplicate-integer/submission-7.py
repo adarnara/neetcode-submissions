7691dcce2423a59cbd4ca8383f48708d3f2d532c
Adarsh Narayanan <94693285+adarnara@users.noreply.github.com>
2026-08-31 13:11:52 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        dup = set()
+
+        for i in range(len(nums)):
+            dup.add(nums[i])
+        
+        if len(dup) < len(nums):
+            return True
+            
+        return False
+
+#two pointer could work too
+        
+        start = 0
+        end = start + 1
+
+        while start < end and end != len(nums) - 1:
+            if nums[start] == nums[end]:
+                return True
+            
+        return False
