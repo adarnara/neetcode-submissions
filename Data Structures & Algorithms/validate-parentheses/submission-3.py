3096b3b9d843239da84ebaa2e41d403c002145f3
Adarsh Narayanan <94693285+adarnara@users.noreply.github.com>
2026-09-07 16:02:39 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+
+        '''
+        #brute force (string manipulation not double iteration or two pointer)
+        # Keep removing valid adjacent pairs until no more can be removed
+        while "()" in s or "[]" in s or "{}" in s:
+            s = s.replace("()", "").replace("[]", "").replace("{}", "")
+        
+        # If valid, all pairs dissolve and s becomes empty
+        return len(s) == 0
+        '''
+
+        stack = list()
+
+        for char in s:
+            if char in ['(', '{', '[']:
+                stack.append(char)
+            
+            if char in [')', '}', ']']:
+                if not bool(stack) or stack[-1] + char not in  ['()', '{}', '[]']:
+                    return False
+                stack.pop()
+            
+        return len(stack) == 0
+
+
+
+            
+            
+
+
+
+
+
+                
